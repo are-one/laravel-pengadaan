d9561c8d12a892aef8789f40b9678920f47bf40f
Arwan Prianto Mangidi <arwanpriantomangidi@gmail.com>
2022-09-17 23:00:23 +0800

    [UPDATE] Model suplier

--- app/Suplier.php
+++ app/Suplier.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Suplier extends Model
 {
-    //
+    protected $table = 'suplier';
+    protected $primaryKey = 'id_suplier';
+    protected $fillable = ['id_suplier', 'nama_usaha', 'email', 'alamat', 'no_npwp', 'password','token', 'status'];
 }
